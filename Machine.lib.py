0c5d06d870601d60cb2479b1d5732f8fec4631cc
MeanMachineMarc <marcus9981@icloud.com>
2022-05-24 13:43:52 +0100

    Machine.lib added

---
+++ Machine.lib.py
@@ -0,0 +1,59 @@
+def sign(num):
+    if num > 0:
+       sign = 1
+    elif num < 0:
+       sign = -1
+    else:
+       sign = 0
+    return sign
+
+def frac_to_dec(numerator,denominator):
+    decimal = float(numerator) / float(denominator)
+    return decimal
+
+def round_3sf(num):
+    
+    if ',' in str(num): #Fraction
+      frac_list = str(num).split(',') 
+      num = FracToDec.frac_to_dec(frac_list[0],frac_list[1])
+    
+    else:
+      pass
+
+    num=float(num)
+    
+    if (num) - int(num) == 0: #Integer
+       if sign.sign(num) == -1: #Negative Integer
+          decimalPlaces = 4 - len(str(int(num)))
+       
+       else: #Zero or Positive Integer
+          decimalPlaces = 3 - len(str(int(num)))
+          
+       rounded = round(num,decimalPlaces)
+       
+    elif int(num) > 0: #Float, > 1
+       len_b4_dp = len(str(int(num)))
+       len_aft_dp = len(str(num))-len_b4_dp - 1
+       decimalPlaces= 4 + len_aft_dp - len(str(num))
+       rounded = round(num,decimalPlaces)
+       
+    else: #Negative Float or Float, < 1
+       hold1 = num 
+       hold2 = abs(num)
+       num = abs(num)
+       count = 0
+       while num < 1:
+             count = count + 1
+             num = 10 * num    
+       
+       if sign.sign(hold1) == -1 and hold2 > 1: #Negative Float, abs > 1
+          len_b4_dp = len(str(int(num)))
+          len_aft_dp = len(str(num)) - len_b4_dp - 1
+          decimalPlaces = 4 + len_aft_dp - len(str(num))
+          rounded = round(hold1,decimalPlaces)
+     
+       else: #Float, abs < 1
+          decimalPlaces = 2 + count 
+          rounded = round(hold1,decimalPlaces)    
+
+    return rounded
