b0b7a2d8b8b90e69566c33fd0d25a361056711b0
MeanMachineMarc <marcus9981@icloud.com>
2022-05-24 12:42:41 +0100

    Comment formatting

--- RoundTo3sf.py
+++ RoundTo3sf.py
@@ -5,13 +5,13 @@ def round_3sf(num):
        decimalPlaces= 3 - len(str(int(num)))
        rounded = round(int(num),decimalPlaces)
        
-    elif int(num) > 0: #Float, >1
+    elif int(num) > 0: #Float, > 1
        len_b4_dp = len(str(int(num)))
        len_aft_dp = len(str(num))-len_b4_dp - 1
        decimalPlaces= 4 + len_aft_dp - len(str(num))
        rounded = round(num,decimalPlaces)
        
-    else: #Negative Float or Float, <1
+    else: #Negative Float or Float, < 1
        hold1 = num 
        hold2 = abs(num)
        num = abs(num)
@@ -20,13 +20,13 @@ def round_3sf(num):
              count = count + 1
              num = 10 * num    
        
-       if sign.sign(hold1) == -1 and hold2 > 1: #Negative Float, abs>1
+       if sign.sign(hold1) == -1 and hold2 > 1: #Negative Float, abs > 1
           len_b4_dp = len(str(int(num)))
           len_aft_dp = len(str(num)) - len_b4_dp - 1
           decimalPlaces = 4 + len_aft_dp - len(str(num))
           rounded = round(hold1,decimalPlaces)
      
-       else: #Float, abs<1
+       else: #Float, abs < 1
           decimalPlaces = 2 + count 
           rounded = round(hold1,decimalPlaces)    
 
