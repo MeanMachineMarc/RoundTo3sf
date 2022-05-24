449edd81e447b85c63cfda670bca2ec4dab973cf
MeanMachineMarc <marcus9981@icloud.com>
2022-05-24 12:37:22 +0100

    Formatting

--- RoundTo3sf.py
+++ RoundTo3sf.py
@@ -1,34 +1,34 @@
 import sign
 
 def round_3sf(num):
-    if (num)-int(num)==0: #Integer
-       decimalPlaces=3-len(str(int(num)))
-       rounded=round(int(num),decimalPlaces)
+    if (num) - int(num) == 0: #Integer
+       decimalPlaces= 3 - len(str(int(num)))
+       rounded = round(int(num),decimalPlaces)
        
-    elif int(num)>0: #Float, >1
-       len_b4_dp=len(str(int(num)))
-       len_aft_dp=len(str(num))-len_b4_dp-1
-       decimalPlaces=4+len_aft_dp-len(str(num))
-       rounded=round(num,decimalPlaces)
+    elif int(num) > 0: #Float, >1
+       len_b4_dp = len(str(int(num)))
+       len_aft_dp = len(str(num))-len_b4_dp - 1
+       decimalPlaces= 4 + len_aft_dp - len(str(num))
+       rounded = round(num,decimalPlaces)
        
     else: #Negative Float or Float, <1
-       hold1=num 
-       hold2=abs(num)
-       num=abs(num)
-       count=0
-       while num<1:
-             count=count+1
-             num=10*num    
+       hold1 = num 
+       hold2 = abs(num)
+       num = abs(num)
+       count = 0
+       while num < 1:
+             count = count + 1
+             num = 10 * num    
        
-       if sign.sign(hold1)==-1 and hold2>1: #Negative Float, abs>1
-          len_b4_dp=len(str(int(num)))
-          len_aft_dp=len(str(num))-len_b4_dp-1
-          decimalPlaces=4+len_aft_dp-len(str(num))
-          rounded=round(hold1,decimalPlaces)
+       if sign.sign(hold1) == -1 and hold2 > 1: #Negative Float, abs>1
+          len_b4_dp = len(str(int(num)))
+          len_aft_dp = len(str(num)) - len_b4_dp - 1
+          decimalPlaces = 4 + len_aft_dp - len(str(num))
+          rounded = round(hold1,decimalPlaces)
      
        else: #Float, abs<1
-          decimalPlaces=2+count 
-          rounded=round(hold1,decimalPlaces)    
+          decimalPlaces = 2 + count 
+          rounded = round(hold1,decimalPlaces)    
 
     return rounded
 
